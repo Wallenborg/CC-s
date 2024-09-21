bd34a07efb859ae9edc951b1fa69172c9bf95eed
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-09-21 19:34:09 +0200

    add info text and new font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Info from "./components/info/Info";
 import NewPage from "./components/newpage/NewPage";
 import PageList from "./components/page-list/PageList";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Info />
       <NewPage />
       <PageList />
     </>
---
+++ src/components/info/Info.jsx
@@ -0,0 +1,15 @@
+import "./Info.css";
+
+export default function Info() {
+  return (
+    <div className="info-text-box">
+      <p className="info-text">
+        My name is Niklas Wallenborg, and I&rsquo;m a creative developer and
+        interdisciplinary artist. &quot;Creative Coding Sundays&quot; is a hobby
+        project of mine, aimed at evolving and learning as a creative developer.
+        I&rsquo;ll try to do a new creative coding project each
+        week&mdash;whether big or small&mdash;and share it here every Sunday.
+      </p>
+    </div>
+  );
+}
