0a66fbe2a3488a68c45c1c8a1c6e74475d735581
Niklas Wallenborg <info@niklaswallenborg.com>
2024-09-29 19:57:15 +0200

    add some new style to info

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -3,6 +3,7 @@ import "./Info.css";
 export default function Info() {
   return (
     <div className="info-text-box">
+      <p className="arrow">&#8659;</p>
       <p className="info-text">
         My name is Niklas Wallenborg, and I&rsquo;m a creative developer and
         interdisciplinary artist. &quot;Creative Coding Sundays&quot; is a hobby
