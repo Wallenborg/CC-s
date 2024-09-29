59b7899007c69fa2b16e207784533d0e1cef9405
Niklas Wallenborg <info@niklaswallenborg.com>
2024-09-29 23:51:41 +0200

    add opacity based on scroll to newpage

--- src/components/new-page-box/NewPageBox.jsx
+++ src/components/new-page-box/NewPageBox.jsx
@@ -1,8 +1,31 @@
+import React, { useState, useEffect } from "react";
 import "./NewPageBox.css";
 
 export default function NewPageBox({ post }) {
+  const [postOpacity, setPostOpacity] = useState(0);
+
+  const handleScroll = () => {
+    // Get the current scroll position
+    const scrollY = window.scrollY;
+
+    // Calculate the new opacity based on scroll position
+    // You can adjust the divisor for different opacity rates
+
+    const newPostOpacity = Math.max(0, 0 + scrollY / 800);
+    setPostOpacity(newPostOpacity);
+  };
+
+  useEffect(() => {
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="new-page-box">
+    <div className="new-page-box" style={{ opacity: postOpacity }}>
       <img src={post.image} alt={post.title} className="new-blog-image" />
 
       <h2 className="new-page-title">{post.title}</h2>
